d666e4152fd2e41e39dc0dcf0d5c214029f5d8ac
hennamanao <93893860+hennamanao@users.noreply.github.com>
2022-05-15 18:58:22 +0900

    ユーザー一覧画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,6 +1,21 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   console.log("UserManagement");
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      {/* {[...Array(10)].map(() => ( */}
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="hoge"
+          fullName="hoge hoge"
+        />
+      </WrapItem>
+      {/* ))} */}
+    </Wrap>
+  );
 });
